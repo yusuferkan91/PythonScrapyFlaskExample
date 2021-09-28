47263dc8f72c9d4d0ee5b16f29cfdebcebb5c895
Yusuf ERKAN <yusuf.erkan@resinasoft.com>
2021-09-28 23:37:34 +0300

    Refactor Code

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 import car_search
-import crochet
-crochet.setup()
 import os
-from flask import Flask , render_template, jsonify, request, redirect, url_for
-from scrapy.crawler import CrawlerRunner
+from flask import Flask , render_template, request, redirect, url_for
 import json
-import copy
 
 app = Flask(__name__)
 
-output_data = []
-crawl_runner = CrawlerRunner()
-
-
 @app.route('/')
 def index():
-    color_list = [{'name':'Beige', 'value':'beige'}, {'name':'Black', 'value':'black'}, {'name':'Blue', 'value':'blue'}, {'name':'Brown','value':'brown'}, {'name':'Gold', 'value':'gold'}, {'name':'Gray', 'value':'gray'}, {'name':'Green','value':'green'}, {'name':'Orange', 'value':'orange'}, {'name':'Purple', 'value':'purple'}, {'name':'Red', 'value':'red'}, {'name':'Silver', 'value':'silver'}, {'name':'White', 'value':'white'}, {'name':'Yellow', 'value':'yellow'}]
-    transmission_list = [{'name':'Automanual', 'value':'automanual'}, {'name':'Automatic', 'value':'automatic'}, {'name':'CVT','value':'cvt'}, {'name':'Manual', 'value':'manual'}, {'name':'Unknown','value':'unknown'}]
-    x = list(map(str, range(1950,2023)))
-    date = [{'name':str(i)} for i in x]
+    url = "https://www.cars.com/shopping/results/?page_size=50&"
+    color_list, transmission_list = car_search.get_search_options(url)
+    date = [{'name':str(i)} for i in range(1970, 2024)]
+
     return render_template("index.html", date=date,
     color=color_list, transmission=transmission_list)
 
@@ -32,42 +24,28 @@ def submit():
         search_url.append("year_min=" + request.form['min_year']) if request.form['min_year']  else ""
         search_url.append("year_max=" + request.form['max_year']) if request.form['max_year']  else ""
         search_url.append("transmission_slugs[]=" + request.form['transmission']) if request.form['transmission'] else "" 
-
-        global baseURL
-        baseURL = 'https://www.cars.com/shopping/results/'
-        'https://www.cars.com/shopping/results/?exterior_color_slugs[]=black'
         
         if os.path.exists("<data.json.json>"): 
         	os.remove("<data.json.json>")
-
-        url = "list"
-        if len(search_url) > 0:
-            url = "list?" + "&".join(search_url)
+        
+        url = "list" + "&".join(search_url) if len(search_url) > 0 else "list"
 
         return redirect(url_for('scrape',url=url)) 
 
-def result(data):
-    data_json = copy.deepcopy(data)
-    json_count = len(data_json)
-    print(len(data_json))
-    return render_template("result.html", json_count= json_count, json_data=json.dumps(data_json, default=set_default))#,
-
 @app.route("/cars/<string:url>")
-async def scrape(url):
-    start_url = 'https://www.cars.com/shopping/results/'
+def scrape(url):
+    start_url = 'https://www.cars.com/shopping/results/?page_size=50&'
+
     if len(url.split('list')) > 0:
         start_url = start_url + url.split('list')[1]
+
     result_data = car_search.read_page(start_url)
     output_json = {i: result_data[i] for i in range(len(result_data))}
+
     with open('data.json', 'w') as f:
         json.dump(output_json, f)
-    print("===================================")
-    print(len(result_data))
-    res = result(output_json)
-    del result_data
-    del output_json
 
-    return res
+    return render_template("result.html", json_count= len(output_json), json_data=json.dumps(output_json, default=set_default))
 
 def set_default(obj):
     if isinstance(obj, set):
--- car_search.py
+++ car_search.py
@@ -2,50 +2,37 @@ from __future__ import print_function
 import gevent
 from gevent import monkey
 import copy
-# patches stdlib (including socket and ssl modules) to cooperate with other greenlets
 monkey.patch_all()
-
-import requests
-from bs4 import BeautifulSoup
 import urllib.request
 from scrapy.selector import Selector
 
-# Note that we're using HTTPS, so
-# this demonstrates that SSL works
-
 result_list = []
 
-def return_page_response(url, query, temp=[]):
+def return_page_response(url, query):
     response = Selector(text=urllib.request.urlopen(url).read())
-    # print(len(temp))
-    temp = temp + ['https://www.cars.com' + i.extract() for i in response.xpath(query)]
-    if len(temp) < 50:
-        next_page = response.xpath("//div[contains(@class, 'sds-pagination__controls')]/a[contains(@class, 'sds-button sds-button--secondary sds-pagination__control')]/@href")
-        if next_page:
-            temp = temp + return_page_response(url='https://www.cars.com' + next_page[0].extract(),query=query, temp=temp)
-    return temp[:50]
+
+    return ['https://www.cars.com' + i.extract() for i in response.xpath(query)]
 
 def read_page(url):
     global result_list
-    print("reading {}".format(url))
+    
     query = "//div[contains(@class, 'vehicle-card   ')]/a[contains(@class, 'vehicle-card-visited-tracking-link')]/@href"
     res = return_page_response(url, query=query)
+    
+    print("reading {}".format(url))
     print(len(res))
-    jobs_car = [gevent.spawn(read_car, _url_car) for _url_car in res]
 
+    jobs_car = [gevent.spawn(read_car, _url_car) for _url_car in res]
     gevent.wait(jobs_car)
+
     return_data = copy.deepcopy(result_list)
     result_list = []
+
     return return_data
 
 def read_car(url):
-    print(" ")
-    print("=======================================")
-    print(url)
-    print("=======================================")
-    print(" ")
-    data = urllib.request.urlopen(url)
-    response = Selector(text=data.read())
+    response = Selector(text=urllib.request.urlopen(url).read())
+    
     image_url = check_none(response.xpath("//div[contains(@class, 'image-swipe-card')]/img[contains(@class, 'swipe-main-image image-index-0')]/@src"))
     car_price = check_none(response.xpath("//div[contains(@class, 'price-section  ')]/span[contains(@class, 'primary-price')]/text()"))
     car_year = check_none(response.xpath("//div[contains(@class, 'title-section')]/h1[contains(@class, 'listing-title')]/text()")).split(" ")[0]
@@ -54,6 +41,7 @@ def read_car(url):
     info = [i.extract() for i in response.xpath("//dl[contains(@class, 'fancy-description-list')]//dd/text()")]
     exterior_color = info[0]
     transmission = info[6]
+    
     item= {}
     item["image_url"]= image_url
     item["car_price"]= car_price
@@ -62,13 +50,25 @@ def read_car(url):
     item["advert_title"] = advert_title
     item["exterior_color"] = exterior_color
     item["transmission"] = transmission
+    
     result_list.append(item)
 
 def check_none(x_path):
     return x_path[0].extract() if len(x_path)>0 else None
 
-def start_read(url):
-    data = requests.get(url)
-    soup=BeautifulSoup(data.content,'lxml')
-    car_url_list = soup.select('div.vehicle-card-visited-tracking-link a')
+def get_search_options(url):
+    response = Selector(text=urllib.request.urlopen(url).read())
 
+    query_color = "//div[contains(@id, 'panel_exterior_colors')]//div[contains(@class, 'sds-checkbox')]//div[contains(@class, 'color-swatch')]/following-sibling::text()"
+    query_transmission = "//div[contains(@id, 'panel_transmissions')]//div[contains(@class, 'sds-checkbox')]//label[contains(@class, 'sds-label')]/text()"
+
+    return parse_query(response, query=query_color), parse_query(response, query=query_transmission)
+
+def parse_query(response, query):
+    lst = []
+    for i in response.xpath(query=query):
+        item = i.extract().replace(" ", "").replace("\n", "")
+        if item:
+            lst.append({"name": item, "value": item.lower()})
+            
+    return lst
